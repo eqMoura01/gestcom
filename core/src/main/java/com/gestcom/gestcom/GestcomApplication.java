9d3d01bd3d2e92b82cc9e6de74e08c6cb5d71477
eqmoura01 <victorlvmora@gmail.com>
2024-09-09 19:55:55 -0300

    first commit

---
+++ core/src/main/java/com/gestcom/gestcom/GestcomApplication.java
@@ -0,0 +1,13 @@
+package com.gestcom.gestcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestcomApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/com/gestcom/gestcom/GestcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestcom.gestcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
