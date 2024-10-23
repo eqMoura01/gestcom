195bbb519605bd4cd97b44d9b7ba6fbb9bc2bf2e
CarusoAugusto <103348874+ragnarcb@users.noreply.github.com>
2024-10-23 19:56:26 -0300

    ADD: Inicial login page (TEST)

---
+++ front/src/pages/login-screen/LoginScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from '../../components/Login'; // Ajuste o caminho conforme necessário
+import './LoginScreen.css'; // Importa o CSS para o LoginScreen
+
+const LoginScreen = () => {
+  return (
+    <main>
+        <div className="login-screen-container">
+        <Login />0
+        </div>
+    </main>
+  );
+};
+
+export default LoginScreen;
